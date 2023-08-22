ae5ffa32c8484395e2e0ea1aba7761aca0342c06
Sarah <201902069@zewailcity.edu.eg>
2023-08-23 00:29:21 +0300

    new feature has been added!

--- hello.py
+++ hello.py
@@ -3,6 +3,9 @@ def main():
     print("Hello,",test)
     age = int(input("Enter your age: "))
     print(f"You are {age} years old")
+    if age>50:
+        print(f"{age}! Too old!")
+
 
 
 main()
