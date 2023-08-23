501fd9f11b9456b47c0f0a9beb381bcec21add99
Sarah <201902069@zewailcity.edu.eg>
2023-08-23 17:19:46 +0300

    python

---
+++ hello.py
@@ -0,0 +1,11 @@
+def main():
+    test=input("Enter your name: ")
+    print("Hello,",test)
+    age = int(input("Enter your age: "))
+    print(f"You are {age} years old")
+    if age>50:
+        print(f"{age}! Too old!")
+
+
+
+main()
